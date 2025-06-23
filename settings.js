204d78827b5e98e9d753101baa2cf66ce64a5824
SARO-YOU <saroyouyou67@gmail.com>
2025-06-23 23:03:55 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,14 @@
+const settings = {
+  packname: 'IT;S MEE',
+  author: 'JACK',
+  botName: "JACK-MD",
+  botOwner: 'JACK', // Your name
+  ownerNumber: '254754977461', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "0.0.1",
+  menuImageUrl: "https://ik.imagekit.io/rmlbayysp/1750708716688-9JVFbGN_pEEgRRiRo.jpg",
+};
+
+module.exports = settings;
